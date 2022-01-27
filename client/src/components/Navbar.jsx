f83c8e188a4a392cf896f968fd1c7dfd7cd024ad
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2022-01-27 15:33:53 +0300

    adв welcom component ad form

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -30,14 +30,14 @@ const Navbar = () => {
             <div className="flex relative">
                 {
                     toggleMenu
-                    ? <AiOutlineClose font-Size={28} className="text-white md:hidden cursor-pointer" onClick={()=> setToggleMenu(false)} />
-                    : <HiMenuAlt4 font-Size={28} className="text-white md:hidden cursor-pointer" onClick={()=> setToggleMenu(true)} />
+                    ? <AiOutlineClose fontSize={28} className="text-white md:hidden cursor-pointer" onClick={()=> setToggleMenu(false)} />
+                    : <HiMenuAlt4 fontSize={28} className="text-white md:hidden cursor-pointer" onClick={()=> setToggleMenu(true)} />
                 }
                 {toggleMenu && (
-                    <ul className="z-10 fixed top-0 right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
+                    <ul className="z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
                       flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in">
                         <li className="text-xl w-full my-2">
-                            <AiOutlineClose onClick={()=> setToggleMenu(false)} />
+                            <AiOutlineClose onClick={()=> setToggleMenu(false)} className="cursor-pointer" />
                             {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index)=>(
                                 <NavbarItem key={item + index} title={item} classProps="my-2 text-lg"/>
                             ))}
